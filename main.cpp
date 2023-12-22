86258234795f6062cad219dd6ad7dc9756e6dd0d
GusevaVarvara <147099563+GusevaVarvara@users.noreply.github.com>
2023-12-22 15:03:56 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,18 +1,70 @@
-#include "urldfa.h"
 #include <iostream>
+#include <string>
+#include <unordered_map>
+
+class FiniteAutomaton {
+private:
+    std::unordered_map<char, int> transitions;
+    std::string substring;
+
+public:
+    FiniteAutomaton(const std::string& substring) : substring(substring) {
+        int currentState = 0;
+
+        for (char ch : substring) {
+            transitions[ch] = currentState++;
+        }
+    }
+
+    void processInput(const std::string& input) {
+        int currentState = 0;
+        bool substringFound = false;
+
+        for (char ch : input) {
+            auto it = transitions.find(ch);
+
+            if (it != transitions.end()) {
+                currentState = it->second + 1;
+            }
+            else {
+                currentState = 0;
+            }
+
+            if (currentState == substring.length()) {
+                std::cout << "All characters in the substring are contained in the string.\n" << std::endl;
+                substringFound = true;
+                break;
+            }
+        }
+
+        if (!substringFound) {
+            std::cout << "Not all characters in a substring are contained in the string.\n" << std::endl;
+        }
+    }
+};
 
 int main() {
-    URLDFA urlDFA;
+    std::cout << "Sample: \n";
 
-    std::cout << "Enter text (type 'end' on a new line to finish input):\n";
+    std::string substring1 = "abc";
+    std::string inputString1 = "ababc";
 
-    std::string inputLine;
-    do {
-        std::getline(std::cin, inputLine);
-        urlDFA.processInput(inputLine);
-    } while (inputLine != "end");
+    FiniteAutomaton automaton1(substring1);
 
-    urlDFA.printURLs();
+    std::cout << "\nSubstring to search: " << substring1 << std::endl;
+    std::cout << "Input string: " << inputString1 << std::endl;
+
+    automaton1.processInput(inputString1);
+
+    std::string inputString, substring;
+    std::cout << "Enter the input string: ";
+    std::getline(std::cin, inputString);
+    std::cout << "Enter the substring: ";
+    std::cin >> substring;
+
+    FiniteAutomaton automaton(substring);
+
+    automaton.processInput(inputString);
 
     return 0;
 }
